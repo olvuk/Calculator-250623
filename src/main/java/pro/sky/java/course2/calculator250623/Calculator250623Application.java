0f0c27d2a52418d7e24cff88d3a9830aec74c5fa
Olga <radenovic-family@yandex.ru>
2023-06-25 15:46:30 +0200

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/calculator250623/Calculator250623Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculator250623;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator250623Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator250623Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/calculator250623/Calculator250623ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculator250623;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator250623ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
