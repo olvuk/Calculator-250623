e5d3c465f14eca05c42b58c423bd3dde5e505991
Olga <radenovic-family@yandex.ru>
2023-06-26 12:39:59 +0200

    Внесены правки

--- src/main/java/pro/sky/java/course2/calculator250623/service/CalculatorService.java
+++ src/main/java/pro/sky/java/course2/calculator250623/service/CalculatorService.java
@@ -4,5 +4,5 @@ public interface CalculatorService {
     int getPlusResult(int num1, int num2);
     int getMinusResult(int num1, int num2);
     int getMultiplyResult(int num1, int num2);
-    int getDivideResult(int num1, int num2);
+    double getDivideResult(int num1, int num2);
 }
--- src/main/java/pro/sky/java/course2/calculator250623/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator250623/service/CalculatorServiceImpl.java
@@ -18,12 +18,12 @@ public class CalculatorServiceImpl implements CalculatorService {
         return num1 * num2;
     }
     @Override
-    public int getDivideResult(int num1, int num2) {
+    public double getDivideResult(int num1, int num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("Divide by zero error");
         }
         else {
-            return num1 / num2;
+            return (double) num1 / num2;
         }
     }
 }
